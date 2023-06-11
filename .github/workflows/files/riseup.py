434b1eaeb7afd6fa686356cec41d424727b7597f
saar.win <saar1122@gmail.com>
2023-06-11 12:21:45 +0300

    test

--- .github/workflows/files/riseup.py
+++ .github/workflows/files/riseup.py
@@ -9,8 +9,11 @@ def main():
     REVIEWER_NAME = os.environ.get('REVIEWER_NAME')
     DESCRIPTION = os.environ.get('DESCRIPTION')
     LINK_TO_PR = os.environ.get('LINK_TO_PR')
+    ACTOR = os.environ.get('ACTOR')
     # get the name of the repo from the event
-    msg = f"Deploying {SERVICE} reviewed by {REVIEWER_NAME} - {DESCRIPTION} [{LINK_TO_PR}]"
+    # get the commit message from the event
+    COMMIT_MESSAGE = os.environ.get('COMMIT_MESSAGE')
+    msg = f"Deploying {SERVICE} reviewed by {REVIEWER_NAME} - {COMMIT_MESSAGE} [{LINK_TO_PR}]"
     res = requests.post("https://hooks.slack.com/services/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", data = json.dumps({"text": msg}))
     if res != 200:
         raise Exception(f"Request failed with status {res.status_code}")
