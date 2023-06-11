a3b5b06a64d8180ae9a504f0a533d70d70f630fb
saar.win <saar1122@gmail.com>
2023-06-11 11:58:09 +0300

    test

--- .github/workflows/files/riseup.py
+++ .github/workflows/files/riseup.py
@@ -5,13 +5,12 @@ import os
 def main():
     '''
     '''
-    context = json.loads(os.getenv("GITHUB_CONTEXT"))
-    service = context.get('repository').get('event').get('repository').get('name')
-    reviewer_name = context['event']
-    description = context['event']
-    link_to_PR = context.get('html_url')
+    SERVICE = os.environ.get('SERVICE')
+    REVIEWER_NAME = os.environ.get('REVIEWER_NAME')
+    DESCRIPTION = os.environ.get('DESCRIPTION')
+    LINK_TO_PR = os.environ.get('LINK_TO_PR')
     # get the name of the repo from the event
-    msg = f"Deploying ${service} reviewed by ${reviewer_name} - ${description} ${link_to_PR}"
+    msg = f"Deploying ${SERVICE} reviewed by ${REVIEWER_NAME} - ${DESCRIPTION} ${LINK_TO_PR}"
     print(msg)
     # res = requests.post("https://hooks.slack.com/services/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", data = json.dumps({"text": msg}))
     # print(res)
